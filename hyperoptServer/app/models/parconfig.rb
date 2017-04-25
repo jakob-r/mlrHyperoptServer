0b4022ae66102da8161329f1551cae3e04f5a365
Jakob Richter <code@jakob-r.de>
2017-04-25 16:24:04 +0200

    vaildation

--- hyperoptServer/app/models/parconfig.rb
+++ hyperoptServer/app/models/parconfig.rb
@@ -1,2 +1,4 @@
 class Parconfig < ApplicationRecord
+  validates :user_email, :json_parconfig, :learner_name, presence: true
+  validates :json_parconfig, :uniqueness => { :scope => :json_parvals } 
 end
