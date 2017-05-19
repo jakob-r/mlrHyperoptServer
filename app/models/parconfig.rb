218ef2a2fe7453b64e6b2075d972e98760dc4445
Jakob Richter <code@jakob-r.de>
2017-05-19 10:40:36 +0200

    anon email

--- app/models/parconfig.rb
+++ app/models/parconfig.rb
@@ -1,4 +1,17 @@
 class Parconfig < ApplicationRecord
   validates :user_email, :json_parconfig, :learner_name, presence: true
   validates :json_parconfig, :uniqueness => { :scope => :json_parvals }
+  
+  def anon_email
+    self.user_email[/.*@.{1}/] + "..."
+  end
+
+  def pretty_parconfig
+    JSON.pretty_generate(JSON.parse(self.json_parconfig))
+  end
+
+  def pretty_parvals
+    JSON.pretty_generate(JSON.parse(self.json_parvals))
+  end
+
 end
