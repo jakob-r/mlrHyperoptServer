462cbf50c0603eb98d5c4c65874d725fc043653b
Jakob Richter <code@jakob-r.de>
2017-06-01 14:21:18 +0200

    implement simple search

--- app/controllers/parconfigs_controller.rb
+++ app/controllers/parconfigs_controller.rb
@@ -6,8 +6,8 @@ class ParconfigsController < ApplicationController
   # GET /parconfigs
   # GET /parconfigs.json
   def index
-    if (params[:key])
-      @parconfigs = Parconfig.where(params[:key] => params[:value])
+    if (search_params.length > 0) 
+      @parconfigs = Parconfig.where(search_params)
     else
       @parconfigs = Parconfig.all
     end
@@ -76,6 +76,10 @@ class ParconfigsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def parconfig_params
       params.require(:parconfig).permit(:user_email, :json_parset, :json_parvals, :learner_class, :learner_type, :learner_name, :download_count, :upload_count, :default, :note)
+    end
+
+    def search_params
+      params.permit(:id, :user_email, :learner_class, :learner_type, :learner_name, :default)
     end
 
     def check_logged_in
